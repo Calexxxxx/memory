45168f3d2f17b35b29c1d62863a3a5e39ad4f0ac
Calexxxxx <pasm28@hotmail.nl>
2018-02-24 20:08:32 +0100

    added comments

--- js/app.js
+++ js/app.js
@@ -68,13 +68,17 @@ let cardDeck = document.getElementById('card-deck');
 // end game modal
 let endgame = document.getElementById('end-game');
 
+// all cards
 let li = document.getElementsByClassName('game__deck__card');
 
+// username for end modal
 let userEnd = localStorage.getItem('username');
+// result text for end modal
 let result = '';
-
+// highscore old to check against score
 let highscoreOld = 0;
 let highscoreResult = 0;
+// amount of points
 let points = 0;
 
 /**
@@ -139,6 +143,10 @@ function add(time) {
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
+/**
+ * @description shuffles the array with cards randomly
+ * @return random shuffled array
+ */
 function shuffle(array) {
   let currentIndex = array.length,
     temporaryValue,
@@ -157,109 +165,175 @@ function shuffle(array) {
  * @description will start the selected game mode
  */
 function startGame() {
+  // remove class from start modal
   modalsPre[1].classList.remove('modal-show');
 
+  // adds fa-star class to the star elements
   starThree.classList.add('fa-star');
   starTwo.classList.add('fa-star');
   starOne.classList.add('fa-star');
 
+  // adds fa-star class to the star elements
   starThreeEnd.classList.add('fa-star');
   starTwoEnd.classList.add('fa-star');
   starOneEnd.classList.add('fa-star');
 
+  // little timeout before game board and score board are shown
   setTimeout(function() {
     game.classList.add('game-show');
     score.classList.add('score-show');
   }, 400);
 
+  // start the timer
   start();
 
+  // make sure moves are 0
   moves = 0;
 
+  // set innerText to moves
   move.innerText = moves;
 
+  // store shuffled cards in temp array
   shuffledArray = shuffle(cards);
 
+  // loop over each card in the array
   for (let i = 0; i < shuffledArray.length; i++) {
+    // create li
     let item = document.createElement('li');
+
+    // create div
     let innerItem = document.createElement('div');
+
+    // set class to li
     item.setAttribute('class', 'game__deck__card');
+
+    // set id to li
     item.setAttribute('id', 'game-card');
+
+    // add img to div as background
     innerItem.style = `background-image: url('${shuffledArray[i]}');`;
+
+    // give each card an id the same as the img name
     innerItem.setAttribute('id', shuffledArray[i].substring(4, 8));
+
+    // add class card to div
     innerItem.setAttribute('class', 'card');
+
+    // add div to li
     item.appendChild(innerItem);
+
+    // add li to ul card-deck
     cardDeck.appendChild(item);
+
+    // add an event listener to the card
     item.addEventListener('click', click);
   }
 }
 
+/**
+ * @description will pass click over to cardClicked used this to removeEventListener once the card is open
+ * Will look for a better solution but this made it work
+ */
 function click(e) {
+  // check if the target that is clicked is the li this because the li has a border if then return and do nothing
   if (e.target.tagName === 'LI') {
     return;
   } else if (e.target.tagName === 'DIV' && e.target.className === 'card') {
+    // else if target is div with class card add the open class to the card
     e.target.parentNode.classList.add('game__deck__card-open');
   }
-
+  // call card clicked
   cardClicked(e.target);
 }
 
+/**
+ * @description will pass click over to cardClicked used this to removeEventListener once the card is open
+ * Will look for a better solution but this made it work
+ */
 function cardClicked(e) {
+  // console.log(e);
+  // push the open card to a temp array
   openCards.push(e);
 
+  // create temp array with all cards
   let cards = document.getElementsByClassName('game__deck__card');
 
+  // if open cards array length is 1 remove the event listener from that card to prevent a double click
   if (openCards.length === 1) {
     openCards[0].parentNode.removeEventListener('click', click);
   } else if (openCards.length === 2) {
+    // temporary remove all event listeners
     for (let i = 0; i < cards.length; i++) {
       cards[i].removeEventListener('click', click);
     }
 
+    // add one to the moves
     moves = moves + 1;
 
+    // update moves inner text
     move.innerText = moves;
 
+    // call count stars to update the scoreboard stars
     countStars(moves);
 
+    // if the two open cards are a match
     if (openCards[0].id === openCards[1].id) {
+      // remove the event listener from the second open card
       openCards[1].parentNode.removeEventListener('click', click);
 
+      // add matched class with timeout to allow turn animation to finish
       setTimeout(function() {
         openCards[0].parentNode.classList.add('matched');
         openCards[1].parentNode.classList.add('matched');
         openCards = [];
       }, 500);
 
+      // move cards to matched cards for the count
       matchedCards.push(openCards[0]);
       matchedCards.push(openCards[1]);
 
+      // add the event listener back to the cards after 400ms and then check for a match in the matched cards array to remove the eventListener again from all cards in the matched array to prevent people from clicking the same pair untill they made 8 matches
       for (let i = 0; i < cards.length; i++) {
         setTimeout(function() {
           cards[i].addEventListener('click', click);
         }, 400);
+        setTimeout(function() {
+          for (let j = 0; j < matchedCards.length; j++) {
+            if (matchedCards[j].id === cards[i].firstChild.id) {
+              cards[i].removeEventListener('click', click);
+            }
+          }
+        }, 500);
       }
 
+      // if the length of matchedCards = 16 this means all possible pairs have been made and the game can be ended
       if (matchedCards.length === 16) {
         endGame(moves);
       }
     } else {
+      // if no match
+      // add event listener back to the cards
       setTimeout(function() {
         for (let i = 0; i < cards.length; i++) {
           cards[i].addEventListener('click', click);
         }
       }, 1000);
 
+      // add class not matched
       setTimeout(function() {
         openCards[0].parentNode.classList.add('not-matched');
         openCards[1].parentNode.classList.add('not-matched');
       }, 500);
 
+      // call close card
       setTimeout(closeCard, 1000);
     }
   }
 }
 
+/**
+ * @description will remove open and not-matched classes and empty the open cards array
+ */
 function closeCard() {
   for (let i = 0; i < openCards.length; i++) {
     openCards[i].parentNode.classList.remove('game__deck__card-open');
@@ -269,12 +343,19 @@ function closeCard() {
   openCards = [];
 }
 
+/**
+ * @description when the user made 8 succesful matches
+ */
 function endGame(moves) {
+  // get the username
   userEnd = localStorage.getItem('username');
 
+  // close the scoreboard
   game.classList.remove('game-show');
+  // close the card deck
   score.classList.remove('score-show');
 
+  // check how many moves the user made if less than 18 3 stars else between 18-22 2 stars and above 23 1 start
   if (moves < 18) {
     // full star
     starThreeEnd.classList.add('fa-star');
@@ -310,8 +391,12 @@ function endGame(moves) {
     points = Math.floor(9000 / moves);
   }
 
+  // get the highscore if there is one
   highscoreOld = localStorage.getItem('highscore');
 
+  // if there is a highscore check if current points are greater than the current highscore
+  // if this is the case set highscoreResult to points else to highscoreOld
+  // and store in localStorage if value is changed
   if (highscoreOld) {
     if (highscoreOld > points) {
       highscoreResult = highscoreOld;
@@ -325,57 +410,80 @@ function endGame(moves) {
 
     localStorage.setItem('highscore', points);
   }
-  console.log(minutes);
 
+  // set only if you played longer than 59 seconds
   if (minutes > 0) {
     document.getElementById('min-end').innerText = minutes + ' minutes ';
   }
+  // set seconds innetText
   document.getElementById('sec-end').innerText = second + ' seconds';
-
+  // set username innerText
   document.getElementById('user-end').innerText = userEnd;
-
+  // set amount of moves made
   document.getElementById('moves-end').innerText = moves;
-
+  // set result string
   document.getElementById('result-text').innerText = result;
-
+  // set highscore innerText
   document.getElementById('highscore').innerText = highscoreResult;
-
+  // set score innerText
   document.getElementById('score-results').innerText = points;
 
+  // show the modal
   setTimeout(function() {
     endgame.classList.add('modal-show');
   }, 400);
 }
 
+/**
+ * @description when called all values are set back to basic values
+ */
 function reset() {
+  // three full stars
   starThree.classList.add('fa-star');
   starTwo.classList.add('fa-star');
   starOne.classList.add('fa-star');
 
+  // empty card deck
   cardDeck.innerHTML = '';
 
+  // empty shuffledArray
   shuffledArray = [];
 
+  // remove the end modal
   endgame.classList.remove('modal-show');
 
+  // empty openCards
   openCards = [];
+  // empty matchedCards
   matchedCards = [];
 
+  // timerTime back to 0
   timerTime = 0;
+
+  // seconds back to 0
   seconds = 0;
 
+  // moves back to 0
   moves = 0;
 
+  // points back to 0
   points = 0;
 
+  // show the card deck and scoreboard
   setTimeout(function() {
     game.classList.add('game-show');
     score.classList.add('score-show');
   }, 400);
+
+  // call start game
   startGame();
+  // start the timer
   start();
 }
 
+/**
+ * @description updates stars on the scoreboard during the game
+ */
 function countStars(moves) {
   if (moves >= 18) {
     starThree.classList.remove('fa-star');
